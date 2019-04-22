e9f269b99c467f182175120e4d5e1a70dab6b0db
mrdemqn <mrdemqnvip@gmail.com>
2019-04-22 15:15:12 +0300

    Work

--- moduleHomeWork/src/com/gmail/mrdemqnvip/arrays/ArrayFive/ArraySum.java
+++ moduleHomeWork/src/com/gmail/mrdemqnvip/arrays/ArrayFive/ArraySum.java
@@ -3,16 +3,48 @@ package com.gmail.mrdemqnvip.arrays.ArrayFive;
 import java.util.Arrays;
 
 public class ArraySum {
-
     public static void main(String[] args) {
-        long[] arrayOne = new long[10];
-        int sum = 0;
-
-        for (int i = 0; i < arrayOne.length; i++) {
-            arrayOne[i] = Math.round(Math.random() * 100);
-        }
-        System.out.println(Arrays.toString(arrayOne));
-
+        boolean isRepeat;
+            int[] array = new int[10];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (int) (100 * Math.random());
+                do {
+                    isRepeat = false;
+                    for (int j = 0; j < i; j++) {
+                        if (array[i] == array[j]) {
+                            array[i] = (int) (100 * Math.random());
+                            isRepeat = true;
+                        }
+                    }
+                } while (isRepeat);
+            }
+            int maxNum = array[0];
+            int minNum = array[0];
+            int maxIndex = 0;
+            int minIndex = 0;
+            int sum = 0;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i]>maxNum) {
+                    maxNum = array[i];
+                    maxIndex = i;
+                }
+                if (array[i]<minNum){
+                    minNum = array[i];
+                    minIndex = i;
+                }
+            }
+            if (maxIndex>minIndex){
+                for (int i = minIndex+1; i<maxIndex; i++){
+                    sum += array[i];
+                }
+            } else {
+                for (int i = maxIndex+1; i<minIndex; i++){
+                    sum += array[i];
+                }
+            }
+            System.out.println("Array of elements: " + Arrays.toString(array));
+            System.out.println("Maximum array element is: " + "[" + maxNum + "]");
+            System.out.println("Minimum array element is: "+ "[" + minNum + "]");
+            System.out.println("The sum of the elements between the maximum and minimum element is " + sum);
     }
-
 }
---
+++ moduleHomeWork/src/com/gmail/mrdemqnvip/arrays/arrayOne/ArrayOne.java
@@ -0,0 +1,44 @@
+package com.gmail.mrdemqnvip.arrays.arrayOne;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayOne{
+    public static void main(String[] args) {
+        Scanner scann = new Scanner(System.in);
+        boolean isTrue = true;
+        while (isTrue) {
+            System.out.println("Enter array size: ");
+            int size;
+            size = Integer.parseInt(scann.nextLine());
+            int[] array = new int[size];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (int) (100 * Math.random());
+            }
+            System.out.println("Random array elements: "+ Arrays.toString(array));
+            int max = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+            System.out.println("The maximum array element is " + max);
+            System.out.println("Do you want to continue?: [Continue] [Exit]");
+
+            switch (scann.nextLine()) {
+                case "Continue": {
+                    System.out.println("Do you want to try again?: [Continue] [Exit]");
+                } break;
+                case "Exit": {
+                    isTrue = false;
+                    break;
+                }
+                default:
+                    System.out.println("Your command is wrong");
+            }
+        }
+        System.gc();
+        System.out.println("Program exit");
+        System.exit(0);
+    }
+}

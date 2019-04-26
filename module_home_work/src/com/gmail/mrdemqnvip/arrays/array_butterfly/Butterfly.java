2924ec062c9b31c04e63816ca61c0393a1809ac0
mrdemqn <mrdemqnvip@gmail.com>
2019-04-26 15:45:25 +0300

    Home work.

--- module_home_work/src/com/gmail/mrdemqnvip/arrays/array_butterfly/Butterfly.java
+++ module_home_work/src/com/gmail/mrdemqnvip/arrays/array_butterfly/Butterfly.java
@@ -4,16 +4,33 @@ public class Butterfly {
 
     public static void main(String[] args) {
 
-        int arrButt[][] = {{1,1,1,1,1},{0,1,1,1,0},{0,0,1,0,0},{0,1,1,1,0},{1,1,1,1,1}};
-        arrButt[0][0] = 1;
-        arrButt[1][1] = 1;
-        arrButt[2][2] = 1;
-        arrButt[3][3] = 1;
-        arrButt[4][4] = 1;
-        for(int i = 0; i<arrButt.length; i++){
-            for(int j = 0; j<arrButt[i].length; j++){
+        int arrButt[][] = {{1, 1, 1, 1, 1}, {0, 1, 1, 1, 0}, {0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}};
+        for (int i = 0; i < arrButt.length; i++) {
+            for (int j = 0; j < arrButt[i].length; j++) {
 
                 System.out.print(arrButt[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        int arrButt2[][] = {{1, 1, 1, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1, 0}, {0, 0, 1, 1, 1, 0, 0},
+                             {0, 0, 0, 1, 0, 0, 0}, {0, 0, 1, 1, 1, 0, 0},
+                              {0, 1, 1, 1, 1, 1, 0}, {1, 1, 1, 1, 1, 1, 1}};
+        for (int i = 0; i < arrButt2.length; i++) {
+            for (int j = 0; j < arrButt2[i].length; j++) {
+
+                System.out.print(arrButt2[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        int arrButt3[][] = {{1, 1, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 0, 1, 1, 1, 1, 1, 0, 0},
+                            {0, 0, 0, 1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0, 0}, {0, 0, 0, 1, 1, 1, 0, 0, 0},
+                             {0, 0, 1, 1, 1, 1, 1, 0, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 0},{1, 1, 1, 1, 1, 1, 1, 1, 1}};
+        for (int i = 0; i < arrButt3.length; i++) {
+            for (int j = 0; j < arrButt3[i].length; j++) {
+
+                System.out.print(arrButt3[i][j] + " ");
             }
             System.out.println();
         }
